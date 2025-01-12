ff9e320cd3c0dd232c3a27a668be2689d5b8ab36
Ronan <ronan.krebel@gmail.com>
2025-01-12 14:11:44 +0100

    Better design => usluge

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import Accordion from 'react-bootstrap/Accordion';
 import axios from 'axios';
-
+import { BiTrash, BiEdit, BiBookOpen} from "react-icons/bi";
 
 
 const Services = () => {
@@ -21,15 +21,22 @@ useEffect(()=>{
 
  function BasicExample() {
   return (
-    <Accordion defaultActiveKey="0">
+    <Accordion defaultActiveKey="0" >
       <Accordion.Item eventKey="0">
         <Accordion.Header>Popis usluga</Accordion.Header>
         <Accordion.Body>
         <ul>
           {usluge.map((item) => 
-            <li key={item.id}>
+            <Row key={item.id} className="border border-secondary rounded shadow-sm py-2 my-2" >
+              <Col md={8}>
               Vrsta usluge - {item.vrsta} - Cijena usluge - {item.cijena}
-            </li>
+              </Col>
+              <Col md={4}>
+              <BiTrash style={{ fontSize: "20px", color: "red", marginRight: "10px"}}/>
+              <BiEdit style={{ fontSize: "20px", color: "black", marginRight: "10px" }}/>
+              <BiBookOpen style={{ fontSize: "20px", color: "black" }}/>
+              </Col>
+              </Row>
           )}
         </ul>
         </Accordion.Body>
@@ -40,8 +47,8 @@ useEffect(()=>{
 
 return(
   <>
-  <Container>Usluge</Container>
-  <Container>
+  <h1 className="mt-3">Usluge</h1>
+  <Container className="px-0 mt-3">
     <BasicExample></BasicExample>
   </Container>
   </>
