0576ae34ac80c56f78843e1d73d1fca0457fe06d
Ronan <ronan.krebel@gmail.com>
2025-01-15 12:23:35 +0100

    Added list of servers

--- src/pages/Servers.jsx
+++ src/pages/Servers.jsx
@@ -3,8 +3,9 @@ import { Container } from "react-bootstrap";
 import Button from 'react-bootstrap/Button';
 import { useState, useEffect } from "react";
 import axios from "axios";
-import Card from 'react-bootstrap/Card';
-
+import { BiTrash, BiEdit, BiBookOpen } from "react-icons/bi";
+import { Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom"
 
 
 const Servers = () => {
@@ -12,52 +13,52 @@ const Servers = () => {
   const [input, setInput] = useState("");
   const [server, setServer] = useState([]);
 
-
-  const fetchServer = async () => {
-    try {
-      const response = await axios.get(`/posluzitelji/${input}`);
-      setServer(response.data[0]);
-      console.log(input)
-      console.log(response.data);
-      console.log("radi se o serveru" + response.data[0].kategorija);
-      
-    } catch (error) {
-      if(error) {
-        throw error;
-      }
-      
-    }
-  }
+  useEffect(() => {
+    axios.get("/posluzitelji")
+      .then((response) => {
+        setServer(response.data)
+        console.log(response.data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }, [])
 
 
-  function ServerCard({ server }) {
-    
-    return (  
-      <Card style={{ width: '18rem' }}>
-        <Card.Body>
-          <Card.Title>{server.kategorija || 'Nema nista za prikazati'}</Card.Title>
-          <Card.Text>
-          {server.naziv || 'Nema nista za prikazati'}
-          </Card.Text>
-          <Button variant="primary">Vidi više</Button>
-        </Card.Body>
-      </Card>
-    );
-  }
+  return (
 
- return(
-    <>
-    <Container>Serveri</Container>
-    <input
-    type="text"
-    value={input}
-    onChange={(e) => setInput(e.target.value)}
-    />
-    <Button onClick={fetchServer}>Dohvati</Button>
     <Container>
-      <ServerCard server={server}/>
+      <h1>Serveri</h1>
+
+    {server && server.length > 0 ?(
+      server.map((srv) => (
+        
+        <Row key={srv.id_posluzitelj} className="border border-secondary rounded shadow-sm py-2 my-2" >
+          <Col md={8}>
+            Kategorija- {srv.kategorija} - rack - {srv.id_rack}
+          </Col>
+          <Col md={4}>
+
+            <BiTrash style={{ fontSize: "20px", color: "red", marginRight: "10px", cursor: "pointer" }} />
+
+            <BiEdit style={{ fontSize: "20px", color: "black", marginRight: "10px" }} />
+            <Link to={`/serveri/${srv.id_posluzitelj}`} style={{ pointerEvents: "auto" }}>
+              <BiBookOpen style={{ fontSize: "20px", color: "black" }} />
+            </Link>
+          </Col>
+        </Row>
+
+
+
+
+))
+):(
+  <Container className="text-danger">Nema servera za prikazati</Container>
+)};
+
+
     </Container>
-    </>
+
   )
 }
 
